5cbc6904384a290a67430105d74c2456aaa62bf6
rahulporel51 <rahulporel51@gmail.com>
2024-02-21 13:43:51 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+## Linked List
+
+# Description
+
+i have used linked list which is most basic and fundamental data structures in data structures & algorithm. and also linked list work similar to an array.
+
+# What i want to do..
+
+The main goal was to do this is that linked list have the principal benift over a conventional array is that the list elements can easily be inserted or removed without reallocation of any other elements.
+
+In some programming languages, the size of an array is a concern and one of the ways to overcome that problem and allow dynamically allocated data is using linked lists.
+
+Luckily in JavaScript, arrays aren’t limited to a certain size, and both insertion and deletion can be done trivially at any index using the appropriate built in array method, so you don’t have to think about overcoming those limitations.
+
+So if array size, array insertion and array deletion are not limitations in JavaScript, are linked lists really necessary? The short answer to that is no; however, it’s the simplest of the dynamic data structures and it will give you a solid foundation, so you can understand more complex data structures like graphs and binary trees with more ease.
+
+# Structure of a linked list
+
+A linked list is a linear collection of data elements called nodes that “point” to the next node by means of a pointer.
+
+Each node holds a single element of data and a link or pointer to the next node in the list.
+
+A head node is the first node in the list, a tail node is the last node in the list. Below is a basic representation of a linked list:
+
+[ NODE(head) ] -> [ NODE ] -> [ NODE(tail) ] -> null
+
+for a more explaintion use this website which has plenty of diagram to understand what a linked list are why to use them or not in real world: [website](https://web.archive.org/web/20200217010131/http://www.cs.cmu.edu/~adamchik/15-121/lectures/Linked%20Lists/linked%20lists.html)
+# Linked-List
+# Linked-List
+# Linked-List
---
+++ script.js
@@ -0,0 +1,142 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+  // Insert last node
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+
+    // if empty, make it head
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.size++;
+  }
+
+  // Insert at index
+  insertAt(data, index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    // if first index
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // set current to first
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current; // Node before index
+      count++;
+      current = current.next; // Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
+
+  // Get at index
+  getAt(index) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (count == index) {
+        console.log(current.data);
+      }
+      count++;
+      current = current.next;
+    }
+
+    return null;
+  }
+  // remove at index
+  removeAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    let current = this.head;
+    let previous;
+    let count = 0;
+
+    // Remove first
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+
+    this.size--;
+  }
+
+  // clear list
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
+  // print list data
+  printListData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const ll = new linkedList();
+ll.insertLast("50. Harsh Singh");
+ll.insertLast("54. Jashon");
+
+ll.insertFirst("4. Jaspreet Singh");
+ll.insertFirst("3. Rohan Walker");
+ll.insertFirst("2. Shyam Singh");
+ll.insertFirst("1. Rahul Porel");
+
+ll.clearList(0);
+
+ll.insertAt("2. Ram Sharma", 3);
+ll.removeAt(3);
+ll.printListData();
+
+ll.getAt(3);
